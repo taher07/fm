035751e52759c83d908e037514e74fe5667a82ff
Taher Lunawadi <43928105+taher07@users.noreply.github.com>
2019-08-08 09:40:04 +0530

    manipulate.js with the object perspective

--- Accounting/manipulate.js
+++ Accounting/manipulate.js
@@ -1,65 +1,42 @@
-/*function addCap(op,amt=0,narr)
-{
-    const cur = localStorage.getItem("totalCapAmount")
-    if(op === "add")
-        amt+=parseInt(cur);
-    else
-        amt-=parseInt(cur);
-    localStorage.setItem("totalCapAmount",amt);
-    localStorage.setItem("narration",narr)
+let entry = {
+    folio: 0,
+    narr: "",
+    nature: "",
+    operation: "",
+    amount: 0
 }
-function addExp(op,amt,narr)
-{
-    const cur = localStorage.getItem("totalExpAmount")
-    if(op === "add")
-    {
-        amt+=parseInt(cur);
-        const am = localStorage.getItem("totalCapAmount")
 
-    }
-}*/
-let nature;
-let op;
-let amt;
-let narr;
 function addSomething(e)
 {
     e.preventDefault()
-    nature = document.getElementById("nature").value;
-    op = document.getElementById("operation").value;
-    amt = document.getElementById('amt').value;
-    narr = document.getElementById('narr').value;
-    console.log(nature,op,amt,narr)
-    /*if(nature === "Capital")
+    entry.folio++;
+    entry.amount = parseInt(document.getElementById('amt').value);
+    entry.nature = document.getElementById("nature").value;
+    entry.operation = document.getElementById("operation").value;
+    entry.narr = document.getElementById('narr').value;
+    console.log(entry)
+}
+writeOverHere = entry => {
+    let ob = document.getElementsByClassName("addHere");
+    let today = new Date;
+    console.log(nature,op,amt,narr);
+    if(nature === "Capital")
     {
-        addCap(op,amt,narr);
+        ob.innerHTML = `<tr>
+            <td>${today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear()}</td>
+            <td>${entry.operation}</td>
+            <td></td>
+            <td>${entry.amount}</td>
+            <td>${entry.narr}</td>
+        </tr>`
     }
-    else
-    {
-        addExp(op,amt,narr);
-    }*/
-    writeOverHere = () => {
-        let ob = document.getElementsByClassName("addHere");
-        let today = new Date;
-        console.log(nature,op,amt,narr);
-        if(nature === "Capital")
-        {
-            ob.innerHTML = `<tr>
-                <td>${today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear()}</td>
-                <td>${op}</td>
-                <td></td>
-                <td>${amt}</td>
-                <td>${narr}</td>
-            </tr>`
-        }
-        else {
-            ob.innerHTML = `<tr>
-                <td>${today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear()}</td>
-                <td>${op}</td>
-                <td>${amt}</td>
-                <td></td>
-                <td>${narr}</td>
-            </tr>`
-        }
+    else {
+        ob.innerHTML = `<tr>
+            <td>${today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear()}</td>
+            <td>${entry.operation}</td>
+            <td>${entry.amount}</td>
+            <td></td>
+            <td>${entry.narr}</td>
+        </tr>`
     }
 }
